010204f6997dda4c8946a3aa9fba09b16fed7b6d
LinMingGG <62881302+LinMingGG@users.noreply.github.com>
2021-09-28 12:10:25 +0800

    Update xixunyun.py

--- xixunyun.py
+++ xixunyun.py
@@ -48,7 +48,7 @@ def mail():
         server.login(fromaddrs, password2)  # 发件人邮箱账号、邮箱密码
         server.sendmail(fromaddrs, [toaddrs, ], msg.as_string())  # 发件人邮箱账号、收件人邮箱账号、发送邮件
         server.quit()  # 关闭连接
-    except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
+   # except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
         #ret = False
     #return ret
 #ret = mail()
