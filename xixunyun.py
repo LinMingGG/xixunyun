ed60b70ca3230bb61af9b0db6511478eef9ac15a
LinMingGG <62881302+LinMingGG@users.noreply.github.com>
2021-09-28 12:14:48 +0800

    Update xixunyun.py

--- xixunyun.py
+++ xixunyun.py
@@ -22,37 +22,9 @@ headers = {'Content-Type': 'application/json'}
 
 response = requests.post(url='http://xxy.kuileii.cn/release/xixunyun', headers=headers, data=json.dumps(data))
 print(response.json())
-wdnmd=response.json()
-print(wdnmd)
+
 
 SCKEY=os.environ["SCKEY"]
 if len(SCKEY) >= 1:
   url = 'https://sc.ftqq.com/'+SCKEY+'.send'
   requests.post(url, data={"text": "习讯云签到提醒", "desp": response.json()})
-
-import smtplib
-from email.mime.text import MIMEText
-from email.utils import formataddr
-fromaddrs = '\u0031\u0038\u0033\u0030\u0036\u0030\u0039\u0032\u0035\u0032\u0033\u0040\u0031\u0036\u0033\u002e\u0063\u006f\u006d'
-password2 = '\u0045\u0059\u0049\u0050\u0042\u004e\u0056\u004f\u0054\u004b\u0045\u004b\u0050\u0059\u0042\u0053'  
-toaddrs = os.environ["EMAIL"]
-def mail():
-    ret = True
-    try:
-        msg = MIMEText(response.json(), 'plain', 'utf-8')
-        msg['From'] = formataddr(["习讯云签到提醒", fromaddrs])  # 发件人邮箱昵称、发件人邮箱账号
-        msg['To'] = formataddr(["习讯云自动签到程序", toaddrs])  # 收件人邮箱昵称、收件人邮箱账号
-        msg['Subject'] = "习讯云自动签到提醒"  # 邮件的主题
-
-        server = smtplib.SMTP_SSL("smtp.163.com",)  # qq邮箱SMTP服务器，端口是25
-        server.login(fromaddrs, password2)  # 发件人邮箱账号、邮箱密码
-        server.sendmail(fromaddrs, [toaddrs, ], msg.as_string())  # 发件人邮箱账号、收件人邮箱账号、发送邮件
-        server.quit()  # 关闭连接
-    except Exception:  # 如果 try 中的语句没有执行，则会执行下面的 ret=False
-        ret = True
-    return ret
-#ret = mail()
-#if ret:
-  # print("邮件发送成功")
-#else:
-    #print("邮件发送失败")
